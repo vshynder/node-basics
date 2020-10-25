b778314b7f909873a1a5b751a5caa62dbb2c78e3
Vlad Shynder <shynderv@gmail.com>
2020-10-25 22:08:07 +0200

    avatars?

--- api/users/users.router.js
+++ api/users/users.router.js
@@ -1,5 +1,17 @@
 const { Router } = require("express");
 const Joi = require("joi");
+const multer = require("multer");
+const path = require("path");
+
+const storage = multer.diskStorage({
+  destination: "public/images/",
+  filename: function (req, file, cb) {
+    const ext = path.extname(file.originalname);
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + "-" + uniqueSuffix + ext);
+  },
+});
+const upload = multer({ storage });
 
 const router = Router();
 
@@ -48,5 +60,20 @@ router.get("/users/current", controller.checkToken, (req, res) => {
   }
   res.status(401).json({ message: "Not authorized" });
 });
+
+router.patch(
+  "/users/avatars",
+  controller.checkToken,
+  upload.single("avatar"),
+  async (req, res) => {
+    req.file = {
+      ...req.file,
+      path: `localhost:3000${req.file.path.split("public")[1]}`,
+    };
+    console.log("req.file", req.file);
+    const user = await controller.changeAvatar(req, res);
+    if (user) res.json({ avatarUrl: user.avatarUrl });
+  }
+);
 
 module.exports = router;
